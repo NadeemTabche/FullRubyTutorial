b1deaedb283e1557133715c1914e1da9734d1463
NadeemTabche <132831251+NadeemTabche@users.noreply.github.com>
2023-11-28 04:08:24 +0000

    Create Variable.rb

---
+++ Variable.rb
@@ -0,0 +1,22 @@
+character_name = "Mike"
+age = 17
+gpa = 4.0
+ismale = true
+istall = false
+flaws = nil #has no value
+#STRINGS
+phrase = "Giraffe Academy  "
+puts phrase[0, 3]
+puts phrase.index("ffe") #first letter of string's index
+#NUMBERS
+num = -20.56774
+puts ("MY FAV NUM: " + num.to_s) #num to_s turns int to
+puts num.abs()
+puts num.round()
+puts num.ceil()
+puts num.floor()
+puts Math.log(2.7182818284590452353602874713527) #natural log ln()
+puts 1 + 7
+puts 1.0 + 7
+puts 10 / 7 #div
+puts 10.0 / 7
