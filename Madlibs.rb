37d61c86fade4ae187b31b416a676b41a75a7f0b
NadeemTabche <132831251+NadeemTabche@users.noreply.github.com>
2023-11-28 03:48:47 +0000

    Create Madlibs.rb

---
+++ Madlibs.rb
@@ -0,0 +1,10 @@
+puts("Enter a colour:")
+colour = gets.chomp()
+puts("Enter a plural noun:")
+plural_noun = gets.chomp()
+puts("Enter a celebrity:")
+celebrity = gets.chomp()
+
+puts("roses are " + colour)
+puts(plural_noun + " are blue")
+puts("I love " + celebrity)
